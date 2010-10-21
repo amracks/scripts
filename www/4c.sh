12c986824cc0d7a4c5cc19002fe8c9347d63c9d9
amarks <amarks@cubemonster.localdomain>
2010-10-21 13:39:51 -0700

    4chan get script

---
+++ www/4c.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+i=1
+lf=list.txt
+
+board=${1}
+
+function image() {
+    name=`echo ${1} | awk -F\/ '{print $NF}'`
+    trd=`echo ${2} | awk -F\/ '{print $NF}'`
+    if [ `grep $name $lf | wc -l` -lt 1 ]; then
+        echo "get ${1}"
+        wget -q -O ${trd}-${name} ${1} &
+        echo ${name} >> $lf
+    else
+        echo "skipping ${1}"
+    fi
+}
+
+function thread() {
+    echo "process thread ${1}"
+    wget -q -O thread.htm http://boards.4chan.org/${board}/${1}
+    images=` awk '{print $11}' thread.htm | grep 'images\.4' | awk -F\" '{print $2}' | grep jpg`
+    for img in ${images}
+    do
+        image ${img} ${1}
+    done
+}
+
+wget -q -O i0.htm http://boards.4chan.org/${1}
+
+while [ ${i} -lt ${2} ]
+do
+    echo "get page ${i}"
+    wget -q -O i${i}.htm http://boards.4chan.org/${1}/${i}
+    i=$((i + 1))
+done
+
+i=0
+while [ ${i} -lt ${2} ]
+do
+    threads=`awk '{print $12}' i${i}.htm | grep Reply | awk -F\" '{print $2}'`
+    for t in $threads
+    do
+        thread $t
+    done
+    i=$((i + 1))
+done
+
+rm *.htm
