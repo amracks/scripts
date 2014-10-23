15e60352745af93f119d4fd042703b0acbc68b21
User Amarks <amarks@ridge.amrx.net>
2014-10-23 16:53:31 -0700

    Packages for FreeBSD Server

---
+++ Server.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+packages="\
+ ImageMagick\
+ android-tools-adb\
+ apache-ant\
+ bash\
+ bind-tools\
+ cdparanoia\
+ cdrtools\
+ cscope\
+ ctags\
+ curl\
+ dvd+rw-tools\
+ eject\
+ fusefs-ntfs\
+ git\
+ hs-haskell-platform\
+ hs-yesod-bin\
+ i386-wine\
+ links\
+ nginx\
+ nmap\
+ openjdk6\
+ openjdk\
+ samba36\
+ scala\
+ syslinux\
+ tmux\
+ todo\
+ tomcat7\
+ unrar\
+ unzip\
+ vim\
+ virtualbox-ose\
+ weechat\
+ wget\
+ wine-gecko\
+ wine-mono\
+ zsh\
+ postgresql93-server\
+ postgresql93-client\
+"
+
+for p in $packages
+do
+  if [ `pkg info | grep "^$p-" | wc -l` -lt 1 ]
+  then
+    pkg install -y $p
+  fi
+done
