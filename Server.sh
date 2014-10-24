c1bb97fd7bbb2198bc10f6ecce9713b9d787ec48
Andrew Marks <amracks@gmail.com>
2014-10-24 14:41:02 -0700

    Dependancies for gitlab server

--- Server.sh
+++ Server.sh
@@ -4,27 +4,47 @@ packages="\
  ImageMagick\
  android-tools-adb\
  apache-ant\
+ autoconf\
+ automake\
  bash\
  bind-tools\
+ bison\
  cdparanoia\
  cdrtools\
+ cmake\
  cscope\
  ctags\
  curl\
  dvd+rw-tools\
  eject\
  fusefs-ntfs\
+ gcc\
+ gdbm\
  git\
+ gmake\
  hs-haskell-platform\
  hs-yesod-bin\
  i386-wine\
+ icu\
+ libtool\
+ libxml2\
+ libxslt\
+ libyaml\
  links\
  nginx\
  nmap\
  openjdk6\
  openjdk\
+ pkgconf\
+ postgresql93-client\
+ postgresql93-server\
+ python2\
+ readline\
+ redis\
  samba36\
  scala\
+ sqlite3\
+ sudo\
  syslinux\
  tmux\
  todo\
@@ -38,15 +58,37 @@ packages="\
  wine-gecko\
  wine-mono\
  zsh\
- postgresql93-server\
- postgresql93-client\
- redis\
 "
 
+services="\
+ zfs\
+ sshd\
+ powerd\
+ ntpdate\
+ ntpd\
+ samba\
+ tomcat7\
+ nginx\
+ sendmail\
+ redis\
+ postgresql\
+"
+
+#Install all packages
 for p in $packages
 do
-  if [ `pkg info | grep "^$p-" | wc -l` -lt 1 ]
-  then
-    pkg install -y $p
-  fi
+    if [ `pkg info | grep "^$p-" | wc -l` -lt 1 ]
+    then
+        pkg install -y $p
+    fi
+done
+
+#Start all services
+for s in $services
+do
+    if [ `grep "^${s}_enable=\"YES\"" /etc/rc.conf | wc -l` -lt 1 ]
+    then
+        echo "${s}_enable=\"YES\"" >> /etc/rc.conf
+        service ${s} start
+    fi
 done
