c1c4ad65e7c6b1759516fa860ee4fdd19d77f8b9
Andrew Marks <amracks@gmail.com>
2014-10-28 15:51:03 -0700

    Added services to desktop

--- Desktop.sh
+++ Desktop.sh
@@ -3,8 +3,8 @@
 #pkg install -y hs-haskell-platform
 
 packages="\
- nmap\
  ImageMagick\
+ android-tools-adb\
  apache-ant\
  bash\
  bind-tools\
@@ -29,38 +29,48 @@ packages="\
  gputils\
  hplip\
  hs-cabal-install\
+ hs-haskell-platform\
+ hs-hoogle\
  hs-xmobar\
- hs-xmonad\
  hs-xmonad-contrib\
+ hs-xmonad\
+ hs-yesod-bin\
  i386-wine\
  icedtea-web\
  links\
  lwjgl\
+ mesa-demos\
  mplayer\
  musicpc\
  musicpd\
  mutt\
  mysql55-client\
+ nmap\
  nvidia-driver\
  nvidia-settings\
- openjdk\
+ okular\
  openjdk6\
+ openjdk\
  pidgin\
+ postgresql93-client\
+ postgresql93-server\
  puppet\
  rexima\
  robotfindskitten\
- ruby\
  ruby20\
+ ruby\
  rxvt-unicode\
  scala\
- slim\
  slim-themes\
+ slim\
+ sudo\
  syslinux\
  terminus-font\
  thunderbird\
  tmux\
  todo\
  uhidd\
+ unrar\
  unzip\
  vim\
  virtualbox-ose\
@@ -73,19 +83,35 @@ packages="\
  xorg\
  xv\
  zsh\
- unrar\
- okular\
- hs-hoogle\
- hs-haskell-platform\
- hs-yesod-bin\
- android-tools-adb\
- mesa-demos\
+"
+services="\
+ zfs\
+ sshd\
+ ntpd\
+ postgresql\
+ dbus\
+ hald\
+ musicpd\
+ hcsecd\
+ sdpd\
+ bthidd\
 "
 
+#Install all packages
 for p in $packages
 do
   if [ `pkg info | grep "^$p-" | wc -l` -lt 1 ]
   then
     pkg install -y $p
   fi
+done
+
+#Start all services
+for s in $services
+do
+    if [ `grep "^${s}_enable=\"YES\"" /etc/rc.conf | wc -l` -lt 1 ]
+    then
+        echo "${s}_enable=\"YES\"" >> /etc/rc.conf
+        service ${s} start
+    fi
 done
