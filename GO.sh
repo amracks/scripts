2f7f97a811dd4044f9c50d7a30267f9935137dba
Andrew Marks <amracks@gmail.com>
2015-10-11 09:27:52 -0700

    Added Oh My ZSH install

--- GO.sh
+++ GO.sh
@@ -3,24 +3,23 @@
 REP_ETC="git@github.com:amracks/etc"
 REP_XMONAD="git@github.com:amracks/xmonad"
 REP_VUNDLE="https://github.com/gmarik/Vundle.vim"
+REP_MCWRAP="git@github.com:amrx/mcjwrapper"
 
 #configure git client
 git config --global user.name "Andrew Marks"
 git config --global user.email "amracks@gmail.com"
 
 #Clone the repo if not exists
-if [ -d "${HOME}/etc" ]
+if [ ! -d "${HOME}/etc" ]
 then
-  cd ${HOME}/etc; git pull
-else
+  echo "Cloning etc"
   git clone ${REP_ETC} ${HOME}/etc
 fi
 
 #Clone the xmonad repo if not exists
-if [ -d "${HOME}/.xmonad" ]
+if [ ! -d "${HOME}/.xmonad" ]
 then
-  cd ${HOME}/.xmonad; git pull
-else
+  echo "Cloning .xmonad"
   git clone ${REP_XMONAD} ${HOME}/.xmonad
   ln -s ${HOME}/.xmonad/xmobarrc ${HOME}/.xmobarrc
 fi
@@ -54,11 +53,46 @@ if [ ! -d "${HOME}/.vim" ]
 then
     mkdir -p ${HOME}/.vim/bundle
     git clone ${REP_VUNDLE} ${HOME}/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
 fi
+
+vim +PluginInstall +qall
 
 # Link Haskell ghci config
 if [ ! -L "${HOME}/.ghci" ]
 then
   ln -s ${HOME}/etc/ghci ${HOME}/.ghci
 fi
+
+# Make source dir
+if [ ! -d "${HOME}/src" ]
+then
+    mkdir ${HOME}/src
+fi
+
+# Clone Minecraft Java Wrapper Script
+if [ ! -d "${HOME}/src/mcjwrapper" ]
+then
+    git clone ${REP_MCWRAP} ${HOME}/src/mcjwrapper
+fi
+
+# Install Oh My ZSH
+if [ ! -d "${HOME}/.oh-my-zsh" ]
+then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+fi
+
+# Symlink .zshrc
+if [ ! -L "${HOME}/.zshrc" ]
+then
+    ln -s ${HOME}/etc/zshrc ${HOME}/.zshrc
+fi
+
+# Setup bin dir
+if [ ! -d "${HOME}/bin" ]
+then
+    mkdir ${HOME}/bin
+    ln -s ${HOME}/scripts/minecraft/Minecraft.sh ${HOME}/bin/minecraft
+    ln -s ${HOME}/src/mcjwrapper/mcjava ${HOME}/bin/mcjava
+fi
+
+
